0973faf444db497109d3ff4f4d4ecf0473e5f3e0
tuxedodiamond17 <leorine17@gmail.com>
2025-02-13 12:33:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/de3b3ymjq/video/upload/v1739421001/Animal_Crossing__New_Horizons_Jazz_Cover_Main_Theme_j3leap.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
