9b51f781d37ed3aa3667f60c2dbbd84bbb891a63
tuxedodiamond17 <leorine17@gmail.com>
2025-02-13 11:59:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/de3b3ymjq/video/upload/v1739419044/Daniel_Caesar_-_Always_Official_Audio_pKFd12id5oQ_sibhmn.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
